44c669abc178089550a5476c7a1e3af6c1601659
rabah babaci <4rabah@gmail.com>
2020-06-30 12:33:33 -0700

    change bg anim mobile scale

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
       minHeight: 200.0,
       minWidth: 200.0,
       scale: 1.0,
-      scaleMobile: 1.5,
+      scaleMobile: 2.0,
       color: 0xffaf3f,
       backgroundColor: 0x0f0e1d,
     });
