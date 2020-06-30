3236add970e7234d9e4c8cc0eefb329dccc36524
rabah babaci <4rabah@gmail.com>
2020-06-30 11:34:54 -0700

    upload an animated background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-function App() {
+export default function App() {
+  useEffect(() => {
+    window.VANTA.GLOBE({
+      el: "#bg-animation",
+      mouseControls: true,
+      touchControls: true,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
+      size: 0.5,
+      color: 0xff3f55,
+      color2: 0x3bf0e5,
+      backgroundColor: 0x111839,
+    });
+  }, []);
+
   return (
-    <div className='App'>
-      <h1>Work in Progress ...</h1>
+    <div className="App">
+      <div id="bg-animation">
+        <h1 id="test">Work in progress...</h1>
+      </div>
     </div>
   );
 }
-
-export default App;
