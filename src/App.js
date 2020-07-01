5061d9fe5fdb71bfbaf72c5f787fc0f018f96337
rabah babaci <4rabah@gmail.com>
2020-06-30 18:02:32 -0700

    seperate the the landing page to its own component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { BsArrowRight } from "react-icons/bs";
+import React from "react";
+import Home from "./pages/Home";
 
 export default function App() {
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    window.VANTA.NET({
-      el: "#bg-animation",
-      mouseControls: false,
-      touchControls: false,
-      minHeight: 200.0,
-      minWidth: 200.0,
-      scale: 1.0,
-      scaleMobile: 1.0,
-      color: 0x2f9baa,
-    });
-
-    setLoaded(true);
-  }, []);
-
   return (
     <div className="App">
-      <div id="bg-animation" />
+      <Home />
 
-      {loaded && (
-        <div className="heading">
-          <h1>
-            Hello, I'm <span>Rabah Babaci</span>.
-            <br />
-            I'm a Full-Stack Web Developer.
-          </h1>
-
-          <button>View my work{<BsArrowRight size={35} />}</button>
-        </div>
-      )}
+      <h1>Under home</h1>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { BsArrowRight } from "react-icons/bs";
+
+export default function App() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    window.VANTA.NET({
+      el: "#bg-animation",
+      mouseControls: false,
+      touchControls: false,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
+      color: 0x2f9baa,
+    });
+
+    setLoaded(true);
+  }, []);
+
+  return (
+    <div className="home">
+      <div id="bg-animation" />
+
+      {loaded && (
+        <div className="heading">
+          <h1>
+            Hello, I'm <span>Rabah Babaci</span>.
+            <br />
+            I'm a Full-Stack Web Developer.
+          </h1>
+
+          <button>View my work{<BsArrowRight size={35} />}</button>
+        </div>
+      )}
+    </div>
+  );
+}
