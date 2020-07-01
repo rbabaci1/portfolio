65c5376d8f7fdcc7af6ad1057277cf740a3471b5
rabah babaci <4rabah@gmail.com>
2020-07-01 16:16:44 -0700

    install material ui

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,30 +1,124 @@
+// import React from "react";
+// import { Link } from "react-scroll";
+
+// import "./navBar.scss";
+
+// export default function NavBar() {
+//   return (
+//     <div className="nav-bar">
+//       <Link to="home" spy={true} smooth={true} duration={700}>
+//         HOME
+//       </Link>
+
+//       <Link to="about" spy={true} smooth={true} duration={700} offset={-55}>
+//         ABOUT
+//       </Link>
+
+//       <Link to="portfolio" spy={true} smooth={true} duration={700} offset={-55}>
+//         PORTFOLIO
+//       </Link>
+
+//       <Link to="blog" spy={true} smooth={true} duration={700} offset={-55}>
+//         BLOG
+//       </Link>
+
+//       <Link to="contact" spy={true} smooth={true} duration={700} offset={0}>
+//         CONTACT
+//       </Link>
+//     </div>
+//   );
+// }
 import React from "react";
-import { Link } from "react-scroll";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
 
-import "./navBar.scss";
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: "auto",
+  },
+});
 
-export default function NavBar() {
+export default function SwipeableTemporaryDrawer() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => event => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = anchor => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === "top" || anchor === "bottom",
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
   return (
-    <div className="nav-bar">
-      <Link to="home" spy={true} smooth={true} duration={700}>
-        HOME
-      </Link>
-
-      <Link to="about" spy={true} smooth={true} duration={700} offset={-55}>
-        ABOUT
-      </Link>
-
-      <Link to="portfolio" spy={true} smooth={true} duration={700} offset={-55}>
-        PORTFOLIO
-      </Link>
-
-      <Link to="blog" spy={true} smooth={true} duration={700} offset={-55}>
-        BLOG
-      </Link>
-
-      <Link to="contact" spy={true} smooth={true} duration={700} offset={0}>
-        CONTACT
-      </Link>
+    <div>
+      {["left", "right", "top", "bottom"].map(anchor => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <SwipeableDrawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            {list(anchor)}
+          </SwipeableDrawer>
+        </React.Fragment>
+      ))}
     </div>
   );
 }
