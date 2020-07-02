ef7cbcebb92786cb6df439cd274b6645a269b3b4
rabah babaci <4rabah@gmail.com>
2020-07-02 10:47:49 -0700

    use react-100vh to fix the mobile view

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { BsArrowRight } from "react-icons/bs";
 import { Link } from "react-scroll";
+import Div100vh from "react-div-100vh";
 
 import "./home.scss";
 
@@ -23,27 +24,29 @@ export default function App() {
   }, []);
 
   return (
-    <div className="home">
-      <div id="bg-animation" />
+    <Div100vh>
+      <div className="home">
+        <div id="bg-animation" />
 
-      {loaded && (
-        <div className="heading">
-          <h1>
-            Hello, I'm <span>Rabah Babaci</span>.
-          </h1>
+        {loaded && (
+          <div className="heading">
+            <h1>
+              Hello, I'm <span>Rabah Babaci</span>.
+            </h1>
 
-          <img
-            src="https://assets-global.website-files.com/5cd091cfb5499f22bdf72905/5e156bcd9ba5cbffdd421c8a_underline-red.png"
-            alt="bottom underline"
-          />
+            <img
+              src="https://assets-global.website-files.com/5cd091cfb5499f22bdf72905/5e156bcd9ba5cbffdd421c8a_underline-red.png"
+              alt="bottom underline"
+            />
 
-          <h1>I'm a Full-Stack Web Developer.</h1>
+            <h1>I'm a Full-Stack Web Developer.</h1>
 
-          <Link to="about" smooth={true} duration={700} offset={-55}>
-            <button>View my work{<BsArrowRight />}</button>
-          </Link>
-        </div>
-      )}
-    </div>
+            <Link to="about" smooth={true} duration={700} offset={-55}>
+              <button>View my work{<BsArrowRight />}</button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </Div100vh>
   );
 }
