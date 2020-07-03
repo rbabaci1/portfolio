8acb1c65344535b2cf298655b94f8316d81088ce
rabah babaci <4rabah@gmail.com>
2020-07-02 20:53:30 -0700

    create reusable mixins for styling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "normalize.css";
-import "./index.scss";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
+import "normalize.css";
+
+import "./styles/index.scss";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
