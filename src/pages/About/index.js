1390f63af74a34e5f682bb883b4a2c3689b8c9b6
rabah babaci <4rabah@gmail.com>
2020-07-03 16:05:41 -0700

    install gsap

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -3,6 +3,14 @@ import React from "react";
 import "./about.scss";
 
 export default function About() {
+  React.useEffect(() => {
+    gsap.to(".header-bar", {
+      x: 400,
+      rotation: 360,
+      duration: 3,
+    });
+  }, []);
+
   return (
     <div className="about">
       <h1>ABOUT</h1>
