a1ebeba1cd26a08cf45a8f9ce521e4f6f5aef2cb
rabah babaci <4rabah@gmail.com>
2020-07-06 10:09:49 -0700

    add animation when rendering the border bottom heading and heading btn

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { BsArrowRight } from "react-icons/bs";
 import { Link } from "react-scroll";
 import Div100vh from "react-div-100vh";
@@ -7,6 +7,9 @@ import Typist from "react-typist";
 import "./homePage.scss";
 
 export default function App() {
+  const [renderImg, setRenderImg] = useState(false);
+  const [renderBtn, setRenderBtn] = useState(false);
+
   const renderCanvas = () => {
     window.VANTA.NET({
       el: "#bg-animation",
@@ -27,62 +30,55 @@ export default function App() {
 
   return (
     <div className="home">
-      <Div100vh data-aos="slide-right" id="bg-animation" />
+      <Div100vh id="bg-animation" />
 
       <div className="heading">
         <Typist
           cursor={{ show: false }}
-          startDelay={800}
-          avgTypingDelay={60}
-          onTypingDone={() => {
-            document.getElementById("border-img").style = {
-              opacity: 1,
-            };
-          }}
+          startDelay={600}
+          avgTypingDelay={65}
+          onTypingDone={() => setRenderImg(true)}
         >
           <h1>
             Hello, I'm <span>Rabah Babaci</span>.
           </h1>
         </Typist>
 
-        <img
-          src="https://assets-global.website-files.com/5cd091cfb5499f22bdf72905/5e156bcd9ba5cbffdd421c8a_underline-red.png"
-          alt="bottom underline"
-          id="border-img"
-          style={{ opacity: 0 }}
-        />
+        {renderImg && (
+          <img
+            src="https://assets-global.website-files.com/5cd091cfb5499f22bdf72905/5e156bcd9ba5cbffdd421c8a_underline-red.png"
+            alt="styles border bottom"
+            data-aos="fade-left"
+          />
+        )}
 
         <Typist
           cursor={{ show: false }}
-          onTypingDone={() => {
-            document.getElementById("view-work-btn").style = {
-              visibility: "visible",
-            };
-
-            renderCanvas();
-          }}
-          avgTypingDelay={60}
+          onTypingDone={() => setRenderBtn(true)}
+          avgTypingDelay={65}
         >
-          <Typist.Delay ms={2500} />
+          <Typist.Delay ms={3000} />
 
           <h1>
-            I'm a Full-Stack West Coast Developer.
-            <Typist.Backspace count={19} delay={500} />
-            <Typist.Delay ms={200} />b Developer.
+            I'm a Full-Stick Web Developer.
+            <Typist.Backspace count={18} delay={500} />
+            <Typist.Delay ms={200} />
+            ack Web Developer.
           </h1>
           <Typist.Delay ms={200} />
         </Typist>
 
-        <Link
-          to="about"
-          smooth={true}
-          duration={600}
-          offset={-55}
-          id="view-work-btn"
-          style={{ visibility: "hidden" }}
-        >
-          <button>View my work{<BsArrowRight />}</button>
-        </Link>
+        {renderBtn && (
+          <Link
+            to="about"
+            smooth={true}
+            duration={600}
+            offset={-55}
+            data-aos="fade-right"
+          >
+            <button>View my work{<BsArrowRight />}</button>
+          </Link>
+        )}
       </div>
     </div>
   );
