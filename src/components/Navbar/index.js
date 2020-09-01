2411808c82b72c97e5c685ee9ce6e9ecccf2438d
rabah babaci <4rabah@gmail.com>
2020-08-31 18:34:46 -0700

    update navbar items active effect

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -110,7 +110,7 @@ export default function SwipeableTemporaryDrawer() {
             open={open}
             onClose={toggleDrawer(false)}
             onOpen={toggleDrawer(true)}
-            transitionDuration={500}
+            transitionDuration={400}
           >
             <AiOutlineClose onClick={toggleDrawer(false)} />
 
