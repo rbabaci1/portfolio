af7072bf9836afbe34a996622e239235934cff71
rabah babaci <4rabah@gmail.com>
2020-08-31 19:23:55 -0700

    create the project title slide effect

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -3,5 +3,14 @@ import React from "react";
 import "./projectCard.scss";
 
 export default function ProjectCard() {
-  return <div className="project-card"></div>;
+  return (
+    <div className="project-card">
+      <div className="border-top" />
+
+      <div className="text">
+        <h2>The Game Of Life</h2>
+        <span>React-JS</span>
+      </div>
+    </div>
+  );
 }
