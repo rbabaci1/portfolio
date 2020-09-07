3e6afa8fe9e8112446cc4cebb4c9919863ed10e9
rabah babaci <4rabah@gmail.com>
2020-09-07 16:31:34 -0700

    update the macbook pro img

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import LearnMoreButton from "../LearnMoreButton";
 import "./projectCard.scss";
-import macBook from "../../images/macbook.png";
+import macBook from "../../images/macBook.png";
 
 export default function ProjectCard({ id, title, subtitles, onClick }) {
   return (
@@ -21,19 +21,4 @@ export default function ProjectCard({ id, title, subtitles, onClick }) {
       </div>
     </div>
   );
-}
-
-{
-  /* <div className="card">
-  <div className="inner-screen">
-    <div className={`bg-img img_${id}`} />
-
-    <div className="text">
-      <h2>The Game Of Life</h2>
-      <span>React-JS</span>
-    </div>
-
-    <LearnMoreButton className="button" onClick={onClick} />
-  </div>{" "}
-</div> */
 }
