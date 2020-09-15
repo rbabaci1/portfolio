5e6f69dd3c9df314adf2184701fc23eb9d53f05f
rabah babaci <4rabah@gmail.com>
2020-09-15 16:04:44 -0700

    re-adjust the home button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -5,13 +5,13 @@ import "./button.scss";
 export default function Button({ text, onClick }) {
   return (
     <div className="button">
-      <button className="learn-more" onClick={onClick}>
+      <div className="learn-more" onClick={onClick}>
         <span className="circle" aria-hidden="true">
           <span className="icon arrow"></span>
         </span>
 
         <span className="button-text">{text}</span>
-      </button>
+      </div>
     </div>
   );
 }
