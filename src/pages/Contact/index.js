46d7561a012a0b087e7ff2f65b928f2274c7d79c
rabah babaci <4rabah@gmail.com>
2020-09-21 16:07:34 -0700

    adjust the demensions of the contact angle img

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -7,14 +7,12 @@ import "./contact.scss";
 export default function Contact() {
   return (
     <div className="contact-container">
-      <div className="angle-container">
-        <img
-          src={angle}
-          alt="angle"
-          data-aos="fade-down"
-          data-aos-duration="500"
-        />
-      </div>
+      <img
+        src={angle}
+        alt="angle"
+        data-aos="fade-down"
+        data-aos-duration="500"
+      />
 
       <div className="contact">
         <h1 data-aos="fade-left" data-aos-duration="700">
