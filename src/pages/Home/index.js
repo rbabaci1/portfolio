dca337d02c1e8db5cf4b7117d20eb06b5aad350a
rabah babaci <4rabah@gmail.com>
2020-09-23 15:33:45 -0700

    update the position of the header border image

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,49 +14,51 @@ export default function App() {
       <div id="bg-animation" />
 
       <div className="heading">
-        <h1>
-          <Typist
-            cursor={{ hideWhenDone: true }}
-            startDelay={500}
-            avgTypingDelay={65}
-            onTypingDone={() => {
-              setRenderImg(true);
-
-              setTimeout(() => {
-                setRenderTitle(true);
-              }, 1000);
-            }}
-          >
-            Hello, I'm <span>Rabah Babaci</span>.
-          </Typist>
-        </h1>
-
-        {renderImg ? (
-          <img
-            src="https://assets-global.website-files.com/5cd091cfb5499f22bdf72905/5e156bcd9ba5cbffdd421c8a_underline-red.png"
-            alt="styles border bottom"
-            data-aos="fade-right"
-            data-aos-duration="500"
-          />
-        ) : undefined}
-
-        {renderTitle ? (
+        <div className="header">
           <h1>
             <Typist
-              cursor={{ hideWhenDone: true, hideWhenDoneDelay: -1000 }}
+              cursor={{ hideWhenDone: true }}
+              startDelay={500}
               avgTypingDelay={65}
-              onTypingDone={() => setRenderBtn(true)}
+              onTypingDone={() => {
+                setRenderImg(true);
+
+                setTimeout(() => {
+                  setRenderTitle(true);
+                }, 1000);
+              }}
             >
-              I'm a Full-Stick Web Developer.
-              <Typist.Backspace count={18} delay={600} />
-              <Typist.Delay ms={200} />
-              ack Web Developer.
-              <Typist.Delay ms={200} />
+              Hello, I'm <span>Rabah Babaci</span>.
             </Typist>
           </h1>
-        ) : undefined}
 
-        {renderBtn ? <Button text="View my work" /> : undefined}
+          {renderImg ? (
+            <img
+              src="https://assets-global.website-files.com/5cd091cfb5499f22bdf72905/5e156bcd9ba5cbffdd421c8a_underline-red.png"
+              alt="styles border bottom"
+              data-aos="fade-right"
+              data-aos-duration="500"
+            />
+          ) : undefined}
+
+          {renderTitle ? (
+            <h1>
+              <Typist
+                cursor={{ hideWhenDone: true, hideWhenDoneDelay: -1000 }}
+                avgTypingDelay={65}
+                onTypingDone={() => setRenderBtn(true)}
+              >
+                I'm a Full-Stick Web Developer.
+                <Typist.Backspace count={18} delay={600} />
+                <Typist.Delay ms={200} />
+                ack Web Developer.
+                <Typist.Delay ms={200} />
+              </Typist>
+            </h1>
+          ) : undefined}
+        </div>
+
+        {renderBtn ? <Button text="View my work" /> : <span id="btn-space" />}
       </div>
     </div>
   );
