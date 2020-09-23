6030e221371d68b59b136452627ee62037d1ee0f
rabah babaci <4rabah@gmail.com>
2020-09-23 11:03:32 -0700

    add a diagonol shape to the contact component

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -7,12 +7,14 @@ import "./contact.scss";
 export default function Contact() {
   return (
     <div className="contact-container">
-      <img
-        src={angle}
-        alt="angle"
-        data-aos="fade-down"
-        data-aos-duration="500"
-      />
+      <img src={angle} alt="diagonol shape" />
+      {/* <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="none"
+      >
+        <polygon fill="#141b2b" points="0,100 100,0 100,100" />
+      </svg> */}
 
       <div className="contact">
         <h1 data-aos="fade-left" data-aos-duration="700">
