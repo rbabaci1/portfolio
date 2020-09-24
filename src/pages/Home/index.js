d9a128ac7e1cb26ce0360cbc28ef54cecfc0a58a
rabah babaci <4rabah@gmail.com>
2020-09-23 20:37:09 -0700

    re-adjust the home heading position

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,24 +14,21 @@ export default function App() {
       <div id="bg-animation" />
 
       <div className="heading">
-        <div className="header">
-          <h1 id="first">
-            <Typist
-              cursor={{ hideWhenDone: true }}
-              startDelay={500}
-              avgTypingDelay={65}
-              onTypingDone={() => {
-                setRenderImg(true);
+        <h1 className="first-header">
+          <Typist
+            cursor={{ hideWhenDone: true }}
+            startDelay={500}
+            avgTypingDelay={65}
+            onTypingDone={() => {
+              setRenderImg(true);
 
-                setTimeout(() => {
-                  setRenderTitle(true);
-                }, 1000);
-              }}
-            >
-              Hello, I'm <span>Rabah Babaci</span>.
-            </Typist>
-          </h1>
-
+              setTimeout(() => {
+                setRenderTitle(true);
+              }, 1000);
+            }}
+          >
+            Hello, I'm <span>Rabah Babaci</span>.
+          </Typist>
           {renderImg ? (
             <img
               src="https://assets-global.website-files.com/5cd091cfb5499f22bdf72905/5e156bcd9ba5cbffdd421c8a_underline-red.png"
@@ -39,24 +36,28 @@ export default function App() {
               data-aos="fade-right"
               data-aos-duration="500"
             />
-          ) : undefined}
+          ) : (
+            <div className="first-header" />
+          )}
+        </h1>
 
-          {renderTitle ? (
-            <h1>
-              <Typist
-                cursor={{ hideWhenDone: true, hideWhenDoneDelay: -1000 }}
-                avgTypingDelay={65}
-                onTypingDone={() => setRenderBtn(true)}
-              >
-                I'm a Full-Stick Web Developer.
-                <Typist.Backspace count={18} delay={600} />
-                <Typist.Delay ms={200} />
-                ack Web Developer.
-                <Typist.Delay ms={200} />
-              </Typist>
-            </h1>
-          ) : undefined}
-        </div>
+        {renderTitle ? (
+          <h1 className="second-header">
+            <Typist
+              cursor={{ hideWhenDone: true, hideWhenDoneDelay: -1000 }}
+              avgTypingDelay={65}
+              onTypingDone={() => setRenderBtn(true)}
+            >
+              I'm a Full-Stick Web Developer.
+              <Typist.Backspace count={18} delay={600} />
+              <Typist.Delay ms={200} />
+              ack Web Developer.
+              <Typist.Delay ms={200} />
+            </Typist>
+          </h1>
+        ) : (
+          <div className="second-header" />
+        )}
 
         {renderBtn ? <Button text="View my work" /> : <span id="btn-space" />}
       </div>
