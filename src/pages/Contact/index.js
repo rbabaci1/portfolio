5c088b8d24e1bc0be176faec81933f9d5895b95e
rabah babaci <4rabah@gmail.com>
2020-10-08 18:08:01 -0700

    add styles and state to contact form

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 import BorderBottom from "../../components/HeaderBottomBorder";
 import DiagonalDiv from "../../components/DiagonalDiv";
 import "./contact.scss";
 
 export default function Contact() {
+  const [formInfo, setFormInfo] = useState({
+    name: "",
+    email: "",
+    message: ""
+  })
+
+  const handleChange = e => {
+    setFormInfo({...formInfo, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = e => {
+    console.log(formInfo)
+    e.preventDefault()
+    setFormInfo({name: "", email:"", message:""})
+  }
+
+
   return (
     <div className="contact-container">
       <DiagonalDiv fill="#232d38" bgColor="#f2f2f2" />
@@ -15,11 +32,36 @@ export default function Contact() {
         </h1>
         <BorderBottom color="white" />
 
-        <div className="form">
-          <input placeholder="Name" type="text" name="name" required/>
-          <input placeholder="Enter Email" type="email" name="email" required/>
-          <textarea placeholder="Your Message..." type="text" name="message" required/>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            value={formInfo.name}
+            onChange={handleChange} 
+            placeholder="Name"
+            type="text"
+            name="name"
+            required 
+          />
+
+          <input
+            value={formInfo.email}
+            onChange={handleChange}
+            placeholder="Enter Email"
+            type="email"
+            name="email"
+            required
+          />
+
+          <textarea
+            value={formInfo.message}
+            onChange={handleChange}
+            placeholder="Your Message..."
+            type="text"
+            name="message"
+            required
+          />
+
+          <button>Submit</button>
+        </form>
 
       </div>
     </div>
