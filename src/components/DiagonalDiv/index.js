9dcc7e6a75957615afdc8e96f6ab6a7855d127e1
rabah babaci <4rabah@gmail.com>
2020-10-08 17:50:10 -0700

    create the contac form main part

--- src/components/DiagonalDiv/index.js
+++ src/components/DiagonalDiv/index.js
@@ -2,19 +2,9 @@ import React from "react";
 
 import "./diagonalDiv.scss";
 
-export default function DiagonalDiv({
-  fill,
-  bgColor,
-  anim = "",
-  delayAnim = "0",
-}) {
+export default function DiagonalDiv({ fill, bgColor}) {
   return (
-    <div
-      data-aos={anim}
-      data-aos-duration="350"
-      data-aos-delay={delayAnim}
-      className="diagonal-container"
-    >
+    <div className="diagonal-container">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 100 100"
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -7,18 +7,20 @@ import "./contact.scss";
 export default function Contact() {
   return (
     <div className="contact-container">
-      <DiagonalDiv
-        fill="#232d38"
-        bgColor="#f2f2f2"
-        anim="slide-right"
-        delayAnim="1200"
-      />
+      <DiagonalDiv fill="#232d38" bgColor="#f2f2f2" />
 
       <div className="contact">
         <h1 data-aos="fade-left" data-aos-duration="700">
           CONTACT
         </h1>
         <BorderBottom color="white" />
+
+        <div className="form">
+          <input placeholder="Name" type="text" name="name" required/>
+          <input placeholder="Enter Email" type="email" name="email" required/>
+          <textarea placeholder="Your Message..." type="text" name="message" required/>
+        </div>
+
       </div>
     </div>
   );
