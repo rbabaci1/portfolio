abdd72145b020390c7ece6b60ef1a9e0444f3fa2
rabah babaci <4rabah@gmail.com>
2020-11-01 14:58:22 -0800

    .

--- src/components/DarkMode/index.js
+++ src/components/DarkMode/index.js
@@ -11,19 +11,9 @@ const DarkModeToggle = () => {
   return (
     <div className="toggle-control" onClick={darkMode.toggle}>
       {darkMode.value ? (
-        <BsMoon
-          size="1.8em"
-          color="#455c69"
-          data-aos="fade-in"
-          data-aos-duration="300"
-        />
+        <BsMoon size="1.8em" color="#455c69" />
       ) : (
-        <HiSun
-          size="1.9em"
-          color="#fff"
-          data-aos="fade-in"
-          data-aos-duration="300"
-        />
+        <HiSun size="1.9em" color="#fff" />
       )}
     </div>
   );
