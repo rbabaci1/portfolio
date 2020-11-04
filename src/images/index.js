225d02506c8619c7e1d33384e567e34daa9000cd
rabah babaci <4rabah@gmail.com>
2020-11-03 17:23:52 -0800

    fix the macbook hover animation

---
+++ src/images/index.js
@@ -0,0 +1,35 @@
+import responsive from "./responsiveIcon.svg";
+import fast from "./speedIcon.svg";
+import cuttingEdge from "./cuttingEdgeIcon.svg";
+import dynamic from "./dynamicIcon.svg";
+import me from "./me.png";
+import js from "./js.svg";
+import react from "./react.svg";
+import git from "./git.svg";
+import html from "./html.svg";
+import css from "./css.svg";
+import gitHub from "./gitHub.svg";
+import nodeJs from "./nodeJs.svg";
+import pg from "./pg.svg";
+import redux from "./redux.svg";
+import sass from "./sass.svg";
+import python from "./python.svg";
+
+export {
+  responsive,
+  fast,
+  cuttingEdge,
+  dynamic,
+  me,
+  js,
+  react,
+  redux,
+  gitHub,
+  git,
+  html,
+  css,
+  pg,
+  sass,
+  nodeJs,
+  python,
+};
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import HexagonCard from "../../components/HexagonCard";
 import BorderBottom from "../../components/HeaderBottomBorder";
+import * as Img from "../../images/index";
 import "./about.scss";
 
 export default function About() {
@@ -17,28 +18,28 @@ export default function About() {
         <HexagonCard
           title="Responsive"
           description="Sensitive, ticklish layouts that will work on any device, big or small."
-          icon={require("../../images/responsiveIcon.svg").default}
+          icon={Img.responsive}
           delay="0"
         />
 
         <HexagonCard
           title="Fast"
           description="Fast load times and lag free interaction, are my highest priority."
-          icon={require("../../images/speedIcon.svg").default}
+          icon={Img.fast}
           delay="100"
         />
 
         <HexagonCard
           title="Cutting edge"
           description="The Modern stage with new styles, techniques applied."
-          icon={require("../../images/cuttingEdgeIcon.svg").default}
+          icon={Img.cuttingEdge}
           delay="200"
         />
 
         <HexagonCard
           title="Dynamic"
           description="Websites don't have to be static. I love making pages come to life."
-          icon={require("../../images/dynamicIcon.svg").default}
+          icon={Img.dynamic}
           delay="300"
         />
       </div>
@@ -46,7 +47,7 @@ export default function About() {
       <div className="bottom-container">
         <div className="about-me-wrapper">
           <img
-            src={require("../../images/me.png").default}
+            src={Img.me}
             alt="rabah's selfie"
             data-aos="fade-up"
             data-aos-duration="400"
@@ -71,61 +72,17 @@ export default function About() {
         <div className="skills-wrapper">
           <h1>SKILLS</h1>
 
-          <img
-            className="js"
-            src={require("../../images/js.svg").default}
-            alt="javascript icon"
-          />
-          <img
-            className="react"
-            src={require("../../images/react.svg").default}
-            alt="react icon"
-          />
-          <img
-            className="html"
-            src={require("../../images/html.svg").default}
-            alt="html icon"
-          />
-          <img
-            className="css"
-            src={require("../../images/css.svg").default}
-            alt="css icon"
-          />
-          <img
-            className="sass"
-            src={require("../../images/sass.svg").default}
-            alt="sass icon"
-          />
-          <img
-            className="redux"
-            src={require("../../images/redux.svg").default}
-            alt="redux icon"
-          />
-          <img
-            className="python"
-            src={require("../../images/python.svg").default}
-            alt="python icon"
-          />
-          <img
-            className="nodeJs"
-            src={require("../../images/nodeJs.svg").default}
-            alt="node js icon"
-          />
-          <img
-            className="pg"
-            src={require("../../images/pg.svg").default}
-            alt="postgreSql icon"
-          />
-          <img
-            className="git"
-            src={require("../../images/git.svg").default}
-            alt="git icon"
-          />
-          <img
-            className="github"
-            src={require("../../images/gitHub.svg").default}
-            alt="gitHub icon"
-          />
+          <img className="js" src={Img.js} alt="javascript icon" />
+          <img className="react" src={Img.react} alt="react icon" />
+          <img className="html" src={Img.html} alt="html icon" />
+          <img className="css" src={Img.css} alt="css icon" />
+          <img className="sass" src={Img.sass} alt="sass icon" />
+          <img className="redux" src={Img.redux} alt="redux icon" />
+          <img className="python" src={Img.python} alt="python icon" />
+          <img className="nodeJs" src={Img.nodeJs} alt="node js icon" />
+          <img className="pg" src={Img.pg} alt="postgreSql icon" />
+          <img className="git" src={Img.git} alt="git icon" />
+          <img className="github" src={Img.gitHub} alt="gitHub icon" />
         </div>
       </div>
     </div>
